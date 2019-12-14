0fc2fa3b59b17825977d8a294124040b4a2ee4d6
stackblock <lambdaTL.anthony@gmail.com>
2019-12-14 18:56:08 -0500

    before refactor

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ const fetchData = async searchTerm => {
       s: searchTerm
     }
   });
-
   if (response.data.Error) {
     return [];
   }
-
   return response.data.Search;
 };
 
@@ -27,10 +25,8 @@ root.innerHTML = `
 const input = document.querySelector("input");
 const dropdown = document.querySelector(".dropdown");
 const resultsWrapper = document.querySelector(".results");
-
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
-
   if (!movies.length) {
     dropdown.classList.remove("is-active");
     return;
@@ -52,14 +48,12 @@ const onInput = async event => {
       input.value = movie.Title;
       onMovieSelect(movie);
     });
-
     resultsWrapper.appendChild(option);
     console.log(movie.Title);
   }
 };
 
 input.addEventListener("input", deBounce(onInput, 1500));
-
 document.addEventListener("click", event => {
   if (!root.contains(event.target)) {
     dropdown.classList.remove("is-active");
@@ -73,5 +67,45 @@ onMovieSelect = async movie => {
       i: movie.imdbID
     }
   });
-  console.log(response.data);
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetail => {
+  console.log(movieDetail);
+  return `
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${movieDetail.Poster}" />
+      </p>
+    </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+    </div>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+    <article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+  </article>
+    <article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Meta Score</p>
+  </article>
+    <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+  </article>
+    <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+  </article>
+  `;
 };
