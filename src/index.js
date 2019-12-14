dd6e770631e020aa3101c7d706a442ac33303f15
stackblock <lambdaTL.anthony@gmail.com>
2019-12-14 17:24:58 -0500

    getting ind movie info

--- src/index.js
+++ src/index.js
@@ -1,29 +1,77 @@
-// Here is your key: 67410f38
-
-// Please append it to all of your API requests,
-
-//OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=67410f38
-
-// Click the following URL to activate your key: http://www.omdbapi.com/apikey.aspx?VERIFYKEY=de1bcb89-7927-441c-a94e-53a315b343c7
-// If you did not make this request, please disregard this email.
-
 const fetchData = async searchTerm => {
-    const response = await axios.get("http://www.omdbapi.com/", {
-      params: {
-        apikey: "67410f38",
-        s: searchTerm
-      }
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "67410f38",
+      s: searchTerm
+    }
+  });
+
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
+};
+
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search for a Movie</b></label>
+<input class="input" />
+<div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+</div>
+`;
+
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+  console.log(event.target.value); // equals 'args[0].target.value' - ^^^^ ABOVE ^^^^ -
+  for (let movie of movies) {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "./images/noImg.jpg" : movie.Poster;
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
+      `;
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+      onMovieSelect(movie);
     });
-  
-    console.log(response.data);
-  };
-  
-  const input = document.querySelector("input");
-  
-  const onInput = event => {
-    fetchData(event.target.value);
-    console.log(event.target.value); // equals 'args[0].target.value' - ^^^^ ABOVE ^^^^ -
-  };
-  
-  input.addEventListener("input", deBounce(onInput, 1500));
-  
+
+    resultsWrapper.appendChild(option);
+    console.log(movie.Title);
+  }
+};
+
+input.addEventListener("input", deBounce(onInput, 1500));
+
+document.addEventListener("click", event => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
+
+onMovieSelect = async movie => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "67410f38",
+      i: movie.imdbID
+    }
+  });
+  console.log(response.data);
+};
---
+++ src/movieApiInfo.js
@@ -0,0 +1,8 @@
+// Here is your key: 67410f38
+
+// Please append it to all of your API requests,
+
+//OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=67410f38
+
+// Click the following URL to activate your key: http://www.omdbapi.com/apikey.aspx?VERIFYKEY=de1bcb89-7927-441c-a94e-53a315b343c7
+// If you did not make this request, please disregard this email.
